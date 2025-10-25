f5e10953adc34efe8ee1158bf679232b5a24b95e
Will Record <william.record94@gmail.com>
2025-10-25 11:51:01 +0100

    Combined curr wave and wind pipelines

--- main.py
+++ main.py
@@ -24,27 +24,92 @@ def convert_msl_to_cd(spot_name, tide_val):
     return tide_val + offset
 
 
-def build_params(spot_name):
-    lat_val = SURF_SPOT_LOCATIONS[spot_name]['latitude']
-    long_val = SURF_SPOT_LOCATIONS[spot_name]['longitude']
-    these_params = {
+def build_curr_api_params(spot_name):
+    """Builds out parameters for all our API calls using the spotname to query the dictionary and then returning the
+    params in a tuple"""
+    # Get LATITUDE and LONGITUDE spot values from dictionary to use in params
+    lat = SURF_SPOT_LOCATIONS[spot_name]['latitude']
+    long = SURF_SPOT_LOCATIONS[spot_name]['longitude']
+    swell_params = build_current_swell_params(lat, long)  # << ------------ Get the swell params
+    wind_params = build_current_wind_params(lat, long)  # << -------------- Get the wind params
+    return swell_params, wind_params
+
+
+def build_current_wind_params(lat_val, long_val):
+    """Uses latitude and longitude as parameters to build a dictionary of parameters needed to query the API"""
+    wind_params = {
+        "latitude": lat_val,
+        "longitude": long_val,
+        "current": ["wind_speed_10m", "wind_direction_10m", "wind_gusts_10m"],
+        "wind_speed_unit": "mph",
+    }
+    return wind_params
+
+
+def build_current_swell_params(lat_val, long_val):
+    """Uses latitude and longitude as parameters to build a dictionary of parameters needed to query the API"""
+    swell_params = {
         "latitude": lat_val,
         "longitude": long_val,
         "current": ["swell_wave_height", "swell_wave_direction", "swell_wave_period", "wind_wave_period",
                     "sea_surface_temperature", "sea_level_height_msl"]
     }
-    return these_params
+    return swell_params
 
 
-def extract(some_params):
-    print(f"...Extracting..")
+def extract_current_data(params_tuple):
+    """Takes a tuple of params and queries the APIs via separate functions for wind and swell.
+    Returns the response objects as a tuple."""
+    swell_params, wind_params = params_tuple  # Unpack Tuple
+    swell_response_obj = extract_current_swell_data(swell_params)
+    wind_response_obj = extract_current_wind_data(wind_params)
+    return swell_response_obj, wind_response_obj
+
+
+def extract_current_swell_data(some_params):
+    print(f"...Extracting current swell data..")
     # Pass the API URL to the get function
-    response = requests.get('https://marine-api.open-meteo.com/v1/marine', params=some_params)
-    print(f"API response code: {response.status_code}")
-    return response
+    swell_response = requests.get('https://marine-api.open-meteo.com/v1/marine', params=some_params)
+    print(f"Current Swell API response code: {swell_response.status_code}")
+    return swell_response
 
 
-def transform(response_obj, spot):
+def extract_current_wind_data(some_params):
+    print(f"...Extracting current wind data..")
+    # # Pass the API URL to the get function
+    wind_response = requests.get("https://api.open-meteo.com/v1/forecast", params=some_params)
+    print(f"Current Wind API response code: {wind_response.status_code}")
+    return wind_response
+
+
+def transform_curr_api_responses(Curr_api_resp_tuple, spot):
+    print(f"...TRANSFORMING API RESPONSES...")
+    swell_resp, wind_resp = Curr_api_resp_tuple  # Unpack Tuple
+    swell_df = transform_curr_swell_response(swell_resp, spot)
+    wind_df = transform_curr_wind_response(wind_resp, spot)
+    print(f"Got to the end!")
+    return swell_df, wind_df
+
+
+def transform_curr_wind_response(wind_resp, spot):
+    response_data = wind_resp.json()  # Use the pandas built in functionality to hopefully make sense of the JSON
+    wind_data = response_data['current']
+    wind_units = response_data['current_units']
+    combined_data = {
+        "wind_speed": wind_data['wind_speed_10m'],
+        "wind_speed_unit": wind_units['wind_speed_10m'],
+        "wind_direction": wind_data['wind_direction_10m'],
+        "wind_direction_unit": wind_units['wind_direction_10m'],
+        "wind_gusts": wind_data['wind_gusts_10m'],
+        "wind_gusts_unit": wind_units['wind_gusts_10m']
+    }
+    print(combined_data)
+    combined_df = pd.DataFrame(data=combined_data, index=[0])
+    print(combined_df)
+    return combined_df
+
+
+def transform_curr_swell_response(response_obj, spot):
     print(f"...Transforming..")
     response_data = response_obj.json()  # Use the pandas built in functionality to hopefully make sense of the JSON
     wave_data = response_data['current']
@@ -70,6 +135,6 @@ def transform(response_obj, spot):
 
 
 spot_name = 'Perranporth'
-params = build_params(spot_name)
-response = extract(params)
-df_result = transform(response, spot_name)
+params_tup = build_curr_api_params(spot_name)
+response_tuple = extract_current_data(params_tup)
+df_result = transform_curr_api_responses(response_tuple, spot_name)
