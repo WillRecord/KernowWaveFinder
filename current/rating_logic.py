8d67d582f1a7d762ff8cbfa2fdc351e5ecdde5b7
Will Record <william.record94@gmail.com>
2025-11-06 10:36:47 +0000

    Looping logic to pull tide_keys from dictionary constants

--- current/rating_logic.py
+++ current/rating_logic.py
@@ -1,27 +1,5 @@
 import pandas as pd
-
-SURF_SPOT_LOCATIONS = {
-    "Perranporth": {"latitude": 50.4165, "longitude": -5.071, 'tide_key': '0546A',
-                    'optimal_wind_dir': 110, 'optimal_swell_dir': 290,
-                    'swell_range_low': 50, 'swell_range_high': 500,
-                    'tidal_range_low': 0, 'tidal_range_high': 7},
-    "Porthleven": {"latitude": 50.06267, "longitude": -5.30733, 'tide_key': 'X',
-                   'optimal_wind_dir': 40, 'optimal_swell_dir': 220,
-                   'swell_range': '50-1000',
-                   'tidal_range_low': 1, 'tidal_range_high': 5},
-    "Praa Sands": {"latitude": 50.1019, "longitude": -5.3945, 'tide_key': 'X',
-                   'optimal_wind_dir': 40, 'optimal_swell_dir': 220,
-                   'swell_range': '100-700',
-                   'tidal_range_low': 0, 'tidal_range_high': 4.5},
-    "Porthmeor": {"latitude": 50.2160, "longitude": -5.5363, 'tide_key': 'X',
-                  'optimal_wind_dir': 110, 'optimal_swell_dir': 290,
-                  'swell_range': '400-2000',
-                  'tidal_range_low': 0, 'tidal_range_high': 4},
-    "Swanpool": {"latitude": 50.1415, "longitude": -5.0712, 'tide_key': 'X',
-                 'optimal_wind_dir': 320, 'optimal_swell_dir': 140,
-                 'swell_range': '50-10000',
-                 'tidal_range_low': 1, 'tidal_range_high': 3.5}
-}
+from current.constants import SURF_SPOT_LOCATIONS
 
 # Test Data
 example_test_data = {'wave_height': 0.7, 'wave_height_unit': 'm',
@@ -145,6 +123,7 @@ def rate_curr_spot(spot, row):
 
 for spot in SURF_SPOT_LOCATIONS.keys():
     print(spot)
+    print(SURF_SPOT_LOCATIONS[spot]['tide_key'])
 
 # <<< UNFINISHED LOGIC AROUND POWER SWEETSPOT
 # def wave_power(wave_height, wave_period):
